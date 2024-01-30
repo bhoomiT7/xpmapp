cb4859a27481cf55fb6bca16ffcaecd6627f02d9
bhoomiT7 <135812959+bhoomiT7@users.noreply.github.com>
2024-01-30 13:15:29 +0530

    changes

--- src/assets/js/event.js
+++ src/assets/js/event.js
@@ -1,15 +1,14 @@
-function showAlert12() {
-  alert("Hello teque7");
-}
-function showAlertexp() {
-  alert("Hello expense category");
-}
-function selectMenuItem(item) {
-  // Remove 'selected' class from all menu items
-  document.querySelectorAll('.menu-item').forEach(function (menuItem) {
-    menuItem.classList.remove('selected');
-  });
 
-  // Add 'selected' class to the clicked menu item
-  item.classList.add('selected');
-}
+  function selectMenuItem(item) {
+    var menuItems = document.querySelectorAll('.menu-item');
+    
+    // Remove 'selected' class from all menu items
+    menuItems.forEach(function (menuItem) {
+      menuItem.classList.remove('selected');
+      menuItem.style.backgroundColor = ''; // Remove any custom background color
+    });
+
+    // Add 'selected' class to the clicked menu item
+    item.classList.add('selected');
+    item.style.backgroundColor = 'red'; // Set the background color to red
+  }
